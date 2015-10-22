2beb814878a5826438aaaa75536850567314d2ab
sgallaghma <stephen.james.gallagher@gmail.com>
2015-10-21 23:06:44 -0400

    added pickle

--- run.py
+++ run.py
@@ -1,9 +1,23 @@
 from steveplan import *
-
+import os.path
 try:
    import cPickle as pickle
 except:
    import pickle
+
+
+
+if os.path.isfile("stored_calendar.p") == True:
+	with open('stored_calendar.p') as f:
+		event_list, task_list, project_dict = pickle.load(f)
+		
+else:
+    event_list = []
+    task_list = []
+    project_dict = {"Unassigned":[None]}
+
+
+
 
 
 print "\n"
@@ -16,19 +30,20 @@ while True:
 					"[t] = make task; [r] = make report; [x] = exit\n") 
 
 	if entry == "x":
+		with open("stored_calendar.p", "wb") as f:
+			pickle.dump([event_list, task_list, project_dict], f)
 		break
 
 	elif entry == "p":
-		make_project()
+		make_project(project_dict)
 	elif entry == "e":
-		make_event()
+		make_event(event_list, project_dict)
 	elif entry == "t":
-		make_task()
+		make_task(task_list, project_dict)
 	elif entry == "r":
-		report()
+		report(task_list,event_list,project_dict)
 	else:
 		print "invalid entry"
 
 
-
-
+	
--- steveplan.py
+++ steveplan.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 
 
@@ -6,11 +7,11 @@ import datetime
 #<------------------------------Global Variables---------------------------------------------->
 
 
-event_list = []
+# event_list = []
 
-task_list = []
+# task_list = []
 
-project_dict = {"Unassigned":[None]}
+# project_dict = {"Unassigned":[None]}
 
 
 
@@ -46,15 +47,16 @@ class Task:
 #<------------------------------Functions----------------------------------------------->
 
 
-def make_project():
+def make_project(project_dict):
 
 	project = raw_input("Please enter the project's name:\n")
 	project_dict[project] = [None]
 	print "Project has been saved.\n"
+	return project_dict
 
 
 
-def make_event():
+def make_event(event_list, project_dict):
 
 	#creates event object
 	description = raw_input("Enter event description:\n") 
@@ -66,19 +68,20 @@ def make_event():
 	a = Event(description,year,month,day,duration)
 
 	#adds event object to event list, re-sorts event list by date
-	global event_list
+	
 	event_list.append(a)
 	event_list.sort(key=lambda x: x.date)
 
-	has_project(a)
+	has_project(a, project_dict)
 
 	print "Event has been saved.\n"
+	return event_list
 
 	
 
 
 
-def make_task():
+def make_task(task_list, project_dict):
 
 	#creates task object
 	description = raw_input("Enter task description:\n") 
@@ -87,7 +90,7 @@ def make_task():
 	a = Task(description,duration)
 
 	#adds task object to task list in order of priority
-	global task_list
+	
 	if len(task_list) == 0:
 		task_list.append(a)
 	elif len(task_list) != 0:
@@ -99,17 +102,17 @@ def make_task():
 		priority = int(raw_input("What priority would you like to assign to this new task? Please enter a positive integer:\n"))
 		task_list.insert(priority-1,a)
 
-	has_project(a)
+	has_project(a, project_dict)
 
 	print "Task has been saved.\n"
+	return task_list
 
 
 
 
 	
-def has_project(item):
+def has_project(item, project_dict):
 
-	global project_dict
 
 	should_assign = (raw_input("Would you like to assign this entry to a project? Enter y/n:\n"))
 
@@ -130,34 +133,39 @@ def has_project(item):
 					print i + "\n"
 		chosen_project = raw_input("Please enter the project you'd like to assign this entry to:\n")
 		if chosen_project not in project_dict:
-			if add_project(chosen_project, item) == True:
-				return
-			elif add_project(chosen_project, item) == False:
-				has_project(item)
-				return
+			should_add = raw_input("This isn't one of the current projects. Would you like to add it as a new project? Enter y/n:\n")
+			if should_add == "y":
+				project_dict[chosen_project] = [item]
+				print "Project and entry added."
+			if should_add == "n":
+				has_project(item, project_dict)
 		elif chosen_project in project_dict:
 			if project_dict[chosen_project] == [None]:
 				project_dict[chosen_project] = [item]
 			else:
 				project_dict[chosen_project].append(item)
 			print "Entry added to project."
+	return project_dict
 
 
-def add_project(project_entered, item):
+def add_project(project_entered, item, project_dict):
 
-	global project_dict
 
 	should_add = raw_input("This isn't one of the current projects. Would you like to add it as a new project? Enter y/n:\n")
 	if should_add == "y":
 		project_dict[project_entered] = [item]
 		print "Project and entry added."
-		return True
+		return project_dict
 	if should_add == "n":
-		return False
+		return project_dict
 
 #<------------------------------Reports----------------------------------------------->
 
-def report():
+def report(task_list,event_list,project_dict):
+
+	print "Generating report....\n"
+	time.sleep(2)
+
 	fin = open("Schedule_Report.txt", "w")
 
 	fin.write("Tasks and Events (by Project):\n")
@@ -175,6 +183,8 @@ def report():
 				fin.write("%s hrs  " % i.duration)
 			fin.write("\n")
 		fin.write("\n") 
+
+	print "Report has been generated. Open 'Schedule_Report.txt' to review.\n"
 
 	fin.close()
 
